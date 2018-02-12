7db12bfab932b3094243f288d250677710f844e4
Zane Sterling <sterling.zane@gmail.com>
2018-02-11 21:32:28 -0500

    Add more logging

--- src/Main.hs
+++ src/Main.hs
@@ -36,11 +36,13 @@ handlers = do
     myPolicy = defaultBodyPolicy "/tmp/" 0 1000 1000
     replyGame = do
       body <- getBody
+      liftIO $ putStrLn $ "got:  " ++ L.unpack body
       let json = decode body :: Maybe Game
       maybe
         (badRequest $ toResponse $ L.pack "Bad request.")
         (\j -> do
-            liftIO $ putStrLn $ show j
+            let response = encode $ playGame j
+            liftIO $ putStrLn $ "sent: " ++ L.unpack response
             ok . toResponse . encode . playGame $ j
         )
         json
